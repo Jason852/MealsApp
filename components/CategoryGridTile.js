37e0293f8c2f769718fc4f0cf34fd9a261803ec3
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-03-06 23:56:06 -0500

    added component

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,15 @@
+import { Pressable, View, Text } from "react-native";
+
+function CategoryGridTile() {
+  return (
+    <View>
+      <Pressable>
+        <View>
+          <Text></Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+export default CategoryGridTile;
