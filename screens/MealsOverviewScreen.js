26e9cdd5d7418b1a2b84d231da8a9e2bdeab20a1
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-03-09 12:39:19 -0500

    routed catId to mealScreen

--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,10 +1,12 @@
 import { View, StyleSheet, Text } from "react-native";
 import { MEALS } from "../data/dummy-data";
 
-function MealsOverviewScreen() {
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId;
+
   return (
     <View style={styles.container}>
-      <Text>Meals Overview Screen</Text>
+      <Text>Meals Overview Screen - {catId}</Text>
     </View>
   );
 }
