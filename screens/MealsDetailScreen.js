eb6f8cc3471bd1728a48150c20ef20312fda6584
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-03-27 15:12:08 -0400

    made title dynamic

---
+++ screens/MealsDetailScreen.js
@@ -0,0 +1,17 @@
+import { View, Text } from "react-native";
+
+function MealsDetailScreen() {
+  return (
+    <View>
+      <Text>This is the meals detail screen</Text>
+    </View>
+  );
+}
+
+export default MealsDetailScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
