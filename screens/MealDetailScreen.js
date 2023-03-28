b137693953255eb65f6401384fd9445eaa22d94e
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-03-28 14:36:16 -0400

    added some styling to mealDetails

--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -10,18 +10,23 @@ function MealDetailScreen({ route }) {
 
   return (
     <View>
-      <Image source={{ uri: selectedMeal.imageUrl }} />
-      <Text>{selectedMeal.title}</Text>
+      <Image style={styles.image} source={{ uri: selectedMeal.imageUrl }} />
+      <Text style={styles.title}>{selectedMeal.title}</Text>
       <MealDetails
         duration={selectedMeal.duration}
         complexity={selectedMeal.complexity}
         affordability={selectedMeal.affordability}
+        textStyle={styles.detailText}
       />
-      <Text>Ingredients</Text>
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>Ingredients</Text>
+      </View>
       {selectedMeal.ingredients.map((ingredient) => (
         <Text key={ingredient}>{ingredient}</Text>
       ))}
-      <Text>Steps</Text>
+      <View style={styles.subtitleContainer}>
+        <Text style={styles.subtitle}>Steps</Text>
+      </View>
       {selectedMeal.steps.map((step) => (
         <Text key={step}>{step}</Text>
       ))}
@@ -32,7 +37,32 @@ function MealDetailScreen({ route }) {
 export default MealDetailScreen;
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
+  image: {
+    width: "100%",
+    height: 350,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 24,
+    margin: 8,
+    textAlign: "center",
+    color: "white",
+  },
+  detailText: {
+    color: "white",
+  },
+  subtitle: {
+    color: "#e2b497",
+    fontSize: 18,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  subtitleContainer: {
+    margin: 4,
+    padding: 6,
+    marginHorizontal: 24,
+    marginVertical: 4,
+    borderBottomColor: "#e2b497",
+    borderBottomWidth: "2",
   },
 });
