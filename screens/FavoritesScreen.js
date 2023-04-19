117342d3ac91831cc1e87bb41fc4c038587dd996
Jason852 <62583852+Jason852@users.noreply.github.com>
2023-04-19 14:04:57 -0400

    reset to pre Context

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,7 +1,8 @@
 import { Text } from "react-native";
 
 function FavoritesScreen() {
-  return <Text>This is favorites Screen!</Text>;
+  return <Text>This is favorites Screen!</Text>
+  <Text>just checking stuff</Text>;
 }
 
 export default FavoritesScreen;
